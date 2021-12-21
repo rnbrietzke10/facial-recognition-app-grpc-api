c8e32feb5b08adf9d889ab4ebe6bc45f033e9c24
Rebecca Brietzke <rnbreitzke@outlook.com>
2021-12-21 11:12:16 -0600

    connect /image endpoint to database and increment entries

--- server.js
+++ server.js
@@ -117,17 +117,14 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.status(200).json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json("No user found with that id");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.status(200).json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Unable to get entries"));
 });
 
 const PORT = process.env.PORT;
