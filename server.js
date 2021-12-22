49c96f9380941f81bde8e2ca0f0ac4ba40009cdb
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-21 22:10:00 -0600

    update database for deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DATABASE_HOST,
     port: 5432,
     user: process.env.DATABASE_USER_NAME,
     password: process.env.DATABASE_PASSWORD,
