74c8753dc9e9348562a2b251f587aa9e6ca3c60b
Rebecca Brietzke <rnbreitzke@outlook.com>
2021-12-21 21:19:41 -0600

    add simple validation to signin and register, move clarifai api call to image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,15 @@
+const clarifai = require("clarifai");
+const app = new Clarifai.App({
+  apiKey: [process.env.CLARIFAI_API_KEY],
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json("Unable to work with API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +24,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
--- server.js
+++ server.js
@@ -52,6 +52,9 @@ app.get("/profile/:id", (req, res) => {
 app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
