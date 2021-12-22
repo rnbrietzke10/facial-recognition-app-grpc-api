7afa420bf18573928a327269a2ac28cc2d59ac08
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-21 22:16:16 -0600

    add database connection

--- server.js
+++ server.js
@@ -11,11 +11,10 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_HOST,
-    port: 5432,
-    user: process.env.DATABASE_USER_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
